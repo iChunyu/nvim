fe13b076974041f3a878fec9db204d1775bd35ab
Chunyu <chunyu2018@foxmail.com>
2022-09-05 09:20:43 +0800

    Replace `omni` (for `vimtex`) with `texlab` (LSP) for completition

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -18,7 +18,7 @@ require('mason-lspconfig').setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
-local servers = { 'clangd', 'pyright', 'ltex', 'sumneko_lua' }
+local servers = { 'ltex', 'texlab', 'clangd', 'pyright', 'sumneko_lua' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
@@ -66,7 +66,6 @@ cmp.setup {
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-        { name = 'omni' },
         { name = 'path' }
     },
 }
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,7 +20,6 @@ require('packer').startup(function(use)
     use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'hrsh7th/cmp-omni' -- Enable `vimtex` autocompletion
     use 'hrsh7th/cmp-path' -- Enable filesystem path autocompletion
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
     use 'L3MON4D3/LuaSnip' -- Snippets plugin
