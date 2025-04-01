86f04102864d244aa107253a1e08bce8cad0de7f
chunyu <chunyu2018@foxmail.com>
2025-04-01 23:19:37 +0800

    add `obsidian.nvim` plugin

--- lua/plugins/writing.lua
+++ lua/plugins/writing.lua
@@ -49,7 +49,36 @@ return {
         end,
         ft = { "markdown" },
         keys = {
-            {'<leader>mp', '<Plug>MarkdownPreviewToggle', 'n', desc = 'Toggle Markdown Preview', { silent = true }}
+            { '<leader>mp', '<Plug>MarkdownPreviewToggle', 'n', desc = 'Toggle Markdown Preview', { silent = true } }
         }
     },
+
+    -- Obsidian support
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        dependencies = { "nvim-lua/plenary.nvim", },
+        opts = {
+            workspaces = {
+                {
+                    name = "Notes",
+                    path = "~/Notes",
+                },
+            },
+            daily_notes = {
+                -- Optional, if you keep daily notes in a separate directory.
+                folder = "DailyNotes",
+                -- Optional, if you want to change the date format for the ID of daily notes.
+                date_format = "%Y-%m-%d",
+                -- Optional, if you want to change the date format of the default alias of daily notes.
+                alias_format = "%B %-d, %Y",
+                -- Optional, default tags to add to each new daily note created.
+                default_tags = { "daily-notes" },
+                -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+                template = nil
+            },
+        },
+    }
 }
